5a01448c0fbca57a224df2b1524975d248b65be2
Lee Lott <llott88@gmail.com>
2025-04-13 17:00:19 -0500

    #1 - Added Registration class.

---
+++ src/Utilities.FileManagement/FileManagementRegistration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Utilities.Gpg;
+using Utilities.IoOperations;
+
+namespace Utilities.FileManagement;
+
+public static class FileManagementRegistration
+{
+	public static IServiceCollection AddFileManagementServices(this IServiceCollection services)
+	{
+		services.AddIoOperationsServices();
+		services.AddGpgServices();
+		return services;
+	}
+}
